90a9c66a0fcfc6c42e659067f16c14fcb96b2290
surendra <surendra@gmail.com>
2021-12-13 10:50:21 +0530

    vechile condtion

---
+++ 03-detailsabout vechicle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#####################################
+# Author:surendra
+# Version:1.1.1
+# Date:13/12/2021
+# Description:This the the main topic
+# Usage: ~/details of vehicle
+#####################################
+
+echo "how is condition?"
+read VEHICLE
+echo "condition,$VEHICLE"
