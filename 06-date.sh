fb14b857747d49b8835485358ad59380c73219c5
surendra <surendra@gmail.com>
2021-12-13 11:40:05 +0530

    date

---
+++ 06-date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#############################
+# Author: surendra
+# Version: 1.2.2
+# Date: 13/12/2021
+# Description: thsi aiajhhs  hjahha"
+# Usage:~/06-date.sh
+##############################
+
+date=today
+
+echo -e "\e[34m hai what is date $date \e[0m"
