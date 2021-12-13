6b79d993e377ccf9494de762bab48f1247baf9d4
surendra <surendra@gmail.com>
2021-12-13 10:56:26 +0530

    about colours implementation

---
+++ 04-colours.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#colour code
+##############################################
+# Author:surendra
+# Version:1.6.0
+# Date:13/12/2021
+# Description:this is nmynn jjsjs
+# Usage: ~/04-colours.sh
+################################################
+
+#syntax: echo -e "\e[colmmessage"
+
+echo -e "\e[31mMy name is surendra"
