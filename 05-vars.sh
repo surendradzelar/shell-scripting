81e8cd59faf9ca75c2b20a66393ed6c59fbb501f
surendra <surendra@gmail.com>
2021-12-13 11:32:56 +0530

    s

--- 05-vars.sh
+++ 05-vars.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+##################################
+# Author:surendra
+# Version:1.2.1
+# Date:13/12/2021
+# Description: this hhsh hhhs shhhs 
+# Usage: ~/05-vars.sh
+
 name=raju
 
 echo -e "\e[34m Hai, $name\e[0m"
